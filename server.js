dac625453278fd7c58dafa3b1bd7e2bf779b7894
cci-loaner <ssharm58@uncc.edu>
2020-10-04 14:15:13 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,58 +1,20 @@
 const express = require('express');
 const app = express();
-const port =3000;
+const port =7000;
 
 app.use('/', express.static('public'));
-
-const budget=
-[ {
-    "title": "Eat out",
-    "budget": 90
-},
-{
-    "title": "Rent",
-    "budget": 450
-               },
-{
-    "title": "Grocery",
-    "budget": 250
-   
-},
-{
-    "title": "Car-Shopping",
-    "budget": 100
-   
-},
-{
-    "title": "HealthInsurance",
-    "budget": 75
-   
-},
-{
-    "title": "Education",
-    "budget": 150
-   
-},
-{
-    "title": "Miscelleneous",
-    "budget": 350
-   
-}
-
-
-
-]
+var budget = require('./server.json');
 app.get('/hello', (req, res)=>{
     res.send('Hello');
 });
 
 app.get('/budget',(req,res)=>{
-
 res.json(budget);
+});
 
-})
+//const jsondata = require("./Users/cci-loaner/personal-budget/server.json");
+
 app.listen(port,() =>{
-console.log('example app');
-
-
+ console.log('example app');
+//console.log(jsondata);
 });
